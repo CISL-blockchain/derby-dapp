7438e61f3a4b1fff88e777f6342cd4edcd69c1d8
lunaticf <724922441@qq.com>
2018-11-04 16:16:27 +0800

    front page built

--- contracts/Travel.sol
+++ contracts/Travel.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Migrations {
-    
+    // 酒店房间
+    struct Room {
+        string hotel;   // 酒店名
+        string roomType;    // 房间类型
+        string fromDate;    // 入住日期 2018-11-3
+        string toDate;      // 离开日期 2018-11-5
+        uint totalPrice;    // 总价格     
+    }
+
+
+    // 用户交易
+    struct UserOrder {
+        string userName;    // 用户名
+        uint time;          // 时间戳 unix
+        Room room;          // 订购房间
+        string OTA;         // 订购的平台
+        string state;       // 表明订单状态：init/comfirmed
+    }
+
+
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,63 @@
 App = {
   web3Provider: null,
   contracts: {},
+  
+  initDatepicker: function () {
+    var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    $('#startDate').datepicker({
+        uiLibrary: 'bootstrap4',
+        iconsLibrary: 'fontawesome',
+        minDate: today,
+        maxDate: function () {
+            return $('#endDate').val();
+        }
+    });
+    $('#endDate').datepicker({
+        uiLibrary: 'bootstrap4',
+        iconsLibrary: 'fontawesome',
+        minDate: function () {
+            return $('#startDate').val();
+        }
+    });
+  },
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    // 初始化时间选择器
+    App.initDatepicker();
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+    // Load hotel.
+    $.getJSON('../hotel.json', function(data) {
+      let hotel = data[0];
+ 
 
-        petsRow.append(petTemplate.html());
+      // 设置酒店名字和信息
+      $('#hotel_name').text(hotel.name);
+      $('#hotel_location').text(hotel.location);
+      $('#hotel_img').attr("src","../images/hotel.jpg");
+
+      $('.alert').alert();
+      $('#startDate').click(function ( ) {
+        $('.alert').alert('close');
+      })
+
+      // 设置酒店房间
+      let roomType = hotel.roomType;
+      let rooms = $('tbody');
+     
+      for (let i = 0; i < roomType.length; i++) {
+        let oneType = $('<tr></tr>');
+        rooms.append(oneType)
+        let roomTypeImg = $('<td></td>').append($('<img/>'));
+        roomTypeImg.find('img').attr('src', roomType[i].imageUrl).attr("class", "img-thumbnail");
+        oneType.append(roomTypeImg)
+        let roomTypeName = $('<td></td>').text(roomType[i].type);
+        oneType.append(roomTypeName);
+        let roomTypePrice = $('<td></td>').text("¥" + roomType[i].price);
+        oneType.append(roomTypePrice);
+        let roomTypeBtn = $('<td></td>');
+        roomTypeBtn.append($('<button>').text("预定").attr("class", "btn btn-success").attr("id", "order-btn"));
+        oneType.append(roomTypeBtn);
+        rooms.append(oneType);
       }
     });
 
